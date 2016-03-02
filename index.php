3b573d8d78f94b8e32e214187c48d3a0a1488990
Nicolas Devynck <nicolas.devynck@live.fr>
2016-03-02 09:25:24 +0100

    trie et creation des .txt

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html lang="fr">
 	<head>
 		<title>Curiculum Vitae Developpeur Web</title>
-
+		<!-- Encodage UTF8 -->
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="Content-Type" content="UTF-8">
+		<!-- Meta pour les robots -->
 		<meta name="Content-Language" content="fr">
 		<meta name="description" content="CV Nicolas Devynck, CV developpeur PHP, Narbonne, Creation de sites, developpement web, XHTML CSS">
-	    <meta name="keywords" content="cv d&eacute;veloppeur narbonne, developpement web, developpeur web, CV freelance, CV developpeur PHP, Narbonne, freelance, indépendant, creation site internet, developpement">
+		<meta name="keywords" content="cv d&eacute;veloppeur narbonne, developpement web, developpeur web, CV freelance, CV developpeur PHP, Narbonne, freelance, indépendant, creation site internet, developpement">
 		<meta name="Copyright" content="Devynck nicolas">
 		<meta name="Author" content="Devynck nicolas">
 		<meta name="Identifier-Url" content="http://www.nicolas-devynck.fr/">
@@ -17,27 +18,32 @@
 		<meta name="Distribution" content="global">
 		<meta name="Geography" content="France">
 		<meta name="Category" content="internet">
-
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<script type="text/javascript" src="jQuery.js"></script>
-		<script type="text/javascript" src="jQueryRotate.js"></script>
-		<link rel="icon" type="image/png" href="logo.png" />
-		<?php include 'conteur.php'; ?>
+		<!-- Liens JQuery plus son plugin, CSS -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="js/jQuery.js"></script>
+		<script type="text/javascript" src="js/jQueryRotate.js"></script>
+		<link rel="icon" type="image/png" href="css/logo.png" />
+		<?php include 'php/conteur.php'; ?>
 
 	</head>
 	<body>
+		<!-- Engrenage JQuery -->
 		<div id="engr1"></div>
 		<div id="engr2"></div>
 		<div id="engr3"></div>
 		<nav>
 			<div class="nav">
+				<!-- Logo -->
 				<div id="logo"></div>
-				<span>[<a class="scrollTo" href="#experiance"> Expériences </a>]</span> <span>[<a class="scrollTo" href="#formation"> Formations </a>]</span> <span>[<a class="scrollTo" href="#info"> Qui-suis-je </a>]</span> <span>[<a class="scrollTo" href="#loisir"> loisirs </a>]</span> <span>[<a class="scrollTo" href="#contact"> Contact </a>]</span>
+				<!-- Menu -->
+				<span>[<a class="scrollTo" href="#experiance"> Expériences </a>]</span><span>[<a class="scrollTo" href="#formation"> Formations </a>]</span><span>[<a class="scrollTo" href="#info"> Qui-suis-je </a>]</span><span>[<a class="scrollTo" href="#loisir"> loisirs </a>]</span><span>[<a class="scrollTo" href="#contact"> Contact </a>]</span>
+				<!-- Engrenage JQuery Bis -->
 				<div id="engr4"></div>
 				<div id="engr5"></div>
 			</div>
 		</nav>
 		<div id="cadre">
+			<!-- Header pour la vertion print (cacher) -->
 			<header>
 				<h1>Nicolas DEVYNCK</h1>
 				www.nicolas-devynck.fr
@@ -45,93 +51,49 @@
 			<aside>
 				<div class="aside">
 					<h2 id="info">Qui-suis-je</h2>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac consequat orci. Nam lectus ante, laoreet in ipsum sit amet, imperdiet porttitor lorem. Donec luctus interdum magna, vitae vehicula massa sagittis quis. Maecenas eu justo viverra, lacinia nisl in, dapibus ipsum. Quisque maximus urna nec dolor hendrerit, vel blandit nunc iaculis. Nullam ut neque ac ligula viverra egestas. Nullam in faucibus massa. Suspendisse eu elementum neque. Etiam ultrices erat a lobortis feugiat.
-					</p>
+					<!-- Qui-suis-je dans txt/info.txt --><?php include 'txt/info.txt'; ?>
 					<a class="scrollTo ancre" href="#cadre">^Haut de page^</a>
 					<h2 id="loisir">loisirs</h2>
-					<p>
-						Voyage en France ou à l'étranger, Electronique (Arduino & Raspberry Pi), Histoire Religieuse.
-					</p>
+					<!-- Loisirs dans txt/loisir.txt --><?php include 'txt/loisir.txt'; ?>
 					<a class="scrollTo ancre" href="#cadre">^Haut de page^</a>
 				</div>
 			</aside>
 			<article>
 				<div class="article">
 					<h2 id="experiance">Expériences</h2>
-					<div class="date">
-						2016
-					</div>
-					<div class="poste">
-						Participation au kidsCoding en partenaria avec  « <a href="http://kidscodingclub.org/" target="_blank">Kids Coding club</a> »
-						<br />
-						<span>Atelier decouverte du pour code informatique à des collégiens.</span>
-					</div>
-					<div class="date">
-						2015
-					</div>
-					<div class="poste">
-						Participation et second lauréat au hackathon organiser par « <a href="http://www.erdf.fr/" target="_blank">ERDF</a> »
-						<br />
-						<span>Equipe de 3 personne pour développer en 2 jour une aplication mobile selont des critaires imposer.</span>
-					</div>
-					<div class="date">
-						2006 2015
-					</div>
-					<div class="poste">
-						Plusieurs contrats dans la restauration sur Narbonne et dans la France.
-						<br />
-						<span>Plusieur Poste à responsabiliter et travail en equipe.
-							<br />Gestion des stock, Creation de fiche technique, Délégation de taches</span>
-					</div>
+					<!-- Expériences dans txt/experiance.txt --><?php include 'txt/experiance.txt'; ?>
 					<a class="scrollTo ancre"  href="#cadre">^Haut de page^</a>
+					
 					<h2 id="formation">Formations</h2>
-					<div class="date">
-						2015 2016
-					</div>
-					<div class="poste">
-						Développement web et application mobile avec « <a href="http://simplonline.co/" target="_blank">Simplon</a> » et « <a href="http://www.legrandnarbonne.com/" target="_blank">Le Grand Narbonne</a> ».
-						<br />
-						<span>Apprentissage langage informatique : PHP, JavaScript, CSS, SQL. de diver framework : JQuery, Bootstrap, Symfony.
-							<br />
-							Utilisation des outis : Git, cRUL, LAMP (Linux, Apache, MySQL, PHP), Cordova .</span>
-					</div>
-					<div class="date">
-						2012 2013
-					</div>
-					<div class="poste">
-						Préparation et validation au diplôme DAEU option scientifique (Diplôme d’accès aux études universitaires) à «<a href="https://www.iutbeziers.fr/" target="_blank"> l’IUT de Béziers</a> ».
-						<br />
-						<span>Bac pour adulte en 4 matières (Maths, Français, Physique et Chimies).</span>
-					</div>
-					<div class="date">
-						2007 2008
-					</div>
-					<div class="poste">
-						Préparation et validation au diplôme CQP restauration (Certificat de qualification professionnelle) en alternance dans la franchise «<a href="http://www.groupelacasa.com/" target="_blank"> Casa Pizza Grill</a> » et « <a href="www.hcconseil.org/">H&amp;C Conseil</a> » de Narbonne.
-						<br />
-						<span>Connaissance des produits et des techniques de préparations
-							<br />
-							Mise en place et dressage sur assiette des entrées desserts, plats, pizzas.</span>
-					</div>
+					<!-- Formations dans txt/formation.txt --><?php include 'txt/formation.txt'; ?>
 					<a class="scrollTo ancre" href="#cadre">^Haut de page^</a>
 					<div class="contact">
 						<h2 id="contact">Contactez moi</h2>
-						<form method="post" action="mail.php" id="formulaire" onsubmit="return verifForm(this)">
+						<!-- Formulaire de contacte -->
+						<form method="post" action="php/mail.php" id="formulaire" onsubmit="return verifForm(this)">
 							<h3>Vos Coordonners :</h3>
 							<div class="input_contact">
-								<label for="nom">Nom & Prénom :</label><br />
-								<input type="text" name="nom" id="nom" required onblur="verifPseudo(this)"/><br />
-								<label for="site">Site Web :</label><br />
-								<input type="url" name="site" id="site" placeholder="http://example.com/" /><br />
-								<label for="mail">Mail :</label><br />
-								<input type="email" name="mail" id="mail" placeholder="contact@example.com" required onblur="verifMail(this)"/><br />
+								<label for="nom">Nom & Prénom :</label>
+								<br />
+								<input type="text" name="nom" id="nom" required onblur="verifPseudo(this)"/>
+								<br />
+								<label for="site">Site Web :</label>
+								<br />
+								<input type="url" name="site" id="site" placeholder="http://example.com/" />
+								<br />
+								<label for="mail">Mail :</label>
+								<br />
+								<input type="email" name="mail" id="mail" placeholder="contact@example.com" required onblur="verifMail(this)"/>
+								<br />
 							</div>
 							<h3>Details du message :</h3>
 							<div class="input_contact">
-								<label for="sujet">Sujet :</label><br />
-								<input type="text" name="sujet" id="sujet" required onblur="verifSujet(this)"/><br />
-								<label for="msg">Message :</label><br />
+								<label for="sujet">Sujet :</label>
+								<br />
+								<input type="text" name="sujet" id="sujet" required onblur="verifSujet(this)"/>
+								<br />
+								<label for="msg">Message :</label>
+								<br />
 								<textarea name="msg" id="msg" required onblur="verifMsg(this)"></textarea>
 								<div class="bouton">
 									<input type="submit" value="Envoyer" />
@@ -145,12 +107,15 @@
 			</article>
 			<footer>
 				<div class="footer">
-					© Devynck nicolas 2015<br />
-    				Les portes de l'avenir sont ouvertes à ceux qui savent les pousser. <a href="https://fr.wikipedia.org/wiki/Coluche" target="_blank">(coluche)</a>
+					<!-- Footer -->
+					© Devynck nicolas 2015
+					<br />
+					Les portes de l'avenir sont ouvertes à ceux qui savent les pousser. <a href="https://fr.wikipedia.org/wiki/Coluche" target="_blank">(coluche)</a>
 				</div>
 				<div class="clear"></div>
 			</footer>
 		</div>
-		<script type="text/javascript" src="script.js"></script>
+		<!-- Script Vérification formulaire, Engrenage, Scroll sur les ancres -->
+		<script type="text/javascript" src="js/script.js"></script>
 	</body>
 </html>
